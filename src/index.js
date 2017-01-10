6862fc5282d519a73e2e95bf5c3141f5e9ab2f15
netkam2 <netkam2@Zalbs-MacBook-Air.local>
2017-01-10 13:12:23 -0500

    class over

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore, applyMiddleware} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 import './index.css';
@@ -13,6 +15,12 @@ const store = createStore(
 )
 
 ReactDOM.render(
-  <App />,
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+
+    </BrowserRouter>,
+
   document.getElementById('root')
 );
